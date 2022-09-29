6a16481ca00b6a592bf354f45ab4cde34237d9f9
FaghriIsaacs <114630247+FaghriIsaacs@users.noreply.github.com>
2022-09-29 21:01:18 +0200

    Add files via upload
    Code Challenge Files

---
+++ PhoneProjectC/frmOldPhone.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace PhoneProjectC
+{
+    partial class frmOldPhone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOldPhone));
+            this.Label5 = new System.Windows.Forms.Label();
+            this.txtOutput2 = new System.Windows.Forms.TextBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnBackspace = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnFour = new System.Windows.Forms.Button();
+            this.txtNumberPressed = new System.Windows.Forms.TextBox();
+            this.txtInstances = new System.Windows.Forms.TextBox();
+            this.txtOutput1 = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(351, 165);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(51, 15);
+            this.Label5.TabIndex = 43;
+            this.Label5.Text = "Send >#";
+            // 
+            // txtOutput2
+            // 
+            this.txtOutput2.Location = new System.Drawing.Point(466, 12);
+            this.txtOutput2.Multiline = true;
+            this.txtOutput2.Name = "txtOutput2";
+            this.txtOutput2.Size = new System.Drawing.Size(173, 168);
+            this.txtOutput2.TabIndex = 42;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(351, 249);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(108, 15);
+            this.Label4.TabIndex = 41;
+            this.Label4.Text = "Character selection";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(351, 220);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(94, 15);
+            this.Label3.TabIndex = 40;
+            this.Label3.Text = "Number pressed";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(351, 278);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(111, 15);
+            this.Label2.TabIndex = 39;
+            this.Label2.Text = "Variable string build";
+            // 
+            // btnZero
+            // 
+            this.btnZero.Location = new System.Drawing.Point(522, 440);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(52, 23);
+            this.btnZero.TabIndex = 38;
+            this.btnZero.Text = "0 _";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+            // 
+            // btnBackspace
+            // 
+            this.btnBackspace.Location = new System.Drawing.Point(467, 440);
+            this.btnBackspace.Name = "btnBackspace";
+            this.btnBackspace.Size = new System.Drawing.Size(50, 23);
+            this.btnBackspace.TabIndex = 37;
+            this.btnBackspace.Text = "*   <=";
+            this.btnBackspace.UseVisualStyleBackColor = true;
+            this.btnBackspace.Click += new System.EventHandler(this.btnBackspace_Click);
+            // 
+            // btnNine
+            // 
+            this.btnNine.Location = new System.Drawing.Point(583, 413);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(56, 23);
+            this.btnNine.TabIndex = 36;
+            this.btnNine.Text = "wxyz 9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Location = new System.Drawing.Point(522, 411);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(52, 23);
+            this.btnEight.TabIndex = 35;
+            this.btnEight.Text = "8 tuv";
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Location = new System.Drawing.Point(467, 410);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(49, 24);
+            this.btnSeven.TabIndex = 34;
+            this.btnSeven.Text = "7 pqrs";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Location = new System.Drawing.Point(582, 385);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(57, 23);
+            this.btnSix.TabIndex = 33;
+            this.btnSix.Text = "mno 6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Location = new System.Drawing.Point(522, 385);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(52, 23);
+            this.btnFive.TabIndex = 32;
+            this.btnFive.Text = "5 jkl";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // Timer1
+            // 
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Location = new System.Drawing.Point(466, 385);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(50, 23);
+            this.btnFour.TabIndex = 31;
+            this.btnFour.Text = "4 ghi";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // txtNumberPressed
+            // 
+            this.txtNumberPressed.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtNumberPressed.Enabled = false;
+            this.txtNumberPressed.Location = new System.Drawing.Point(467, 212);
+            this.txtNumberPressed.Name = "txtNumberPressed";
+            this.txtNumberPressed.Size = new System.Drawing.Size(34, 23);
+            this.txtNumberPressed.TabIndex = 30;
+            // 
+            // txtInstances
+            // 
+            this.txtInstances.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtInstances.Enabled = false;
+            this.txtInstances.Location = new System.Drawing.Point(467, 241);
+            this.txtInstances.Name = "txtInstances";
+            this.txtInstances.Size = new System.Drawing.Size(36, 23);
+            this.txtInstances.TabIndex = 29;
+            // 
+            // txtOutput1
+            // 
+            this.txtOutput1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtOutput1.Enabled = false;
+            this.txtOutput1.Location = new System.Drawing.Point(466, 270);
+            this.txtOutput1.Name = "txtOutput1";
+            this.txtOutput1.Size = new System.Drawing.Size(173, 23);
+            this.txtOutput1.TabIndex = 28;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(583, 442);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(56, 20);
+            this.btnSubmit.TabIndex = 27;
+            this.btnSubmit.Text = ">#";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Location = new System.Drawing.Point(582, 356);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(57, 23);
+            this.btnThree.TabIndex = 26;
+            this.btnThree.Text = "def 3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(522, 356);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(52, 23);
+            this.btnTwo.TabIndex = 25;
+            this.btnTwo.Text = "2 abc";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(466, 356);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(50, 23);
+            this.btnOne.TabIndex = 24;
+            this.btnOne.Text = "1&&\'(";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox1.Image")));
+            this.PictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(333, 582);
+            this.PictureBox1.TabIndex = 23;
+            this.PictureBox1.TabStop = false;
+            // 
+            // frmOldPhone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 599);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.txtOutput2);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnBackspace);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.txtNumberPressed);
+            this.Controls.Add(this.txtInstances);
+            this.Controls.Add(this.txtOutput1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.PictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmOldPhone";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Old Phone";
+            this.Load += new System.EventHandler(this.frmOldPhone_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Label Label5;
+        internal TextBox txtOutput2;
+        internal Label Label4;
+        internal Label Label3;
+        internal Label Label2;
+        internal Button btnZero;
+        internal Button btnBackspace;
+        internal Button btnNine;
+        internal Button btnEight;
+        internal Button btnSeven;
+        internal Button btnSix;
+        internal Button btnFive;
+        internal System.Windows.Forms.Timer Timer1;
+        internal Button btnFour;
+        internal TextBox txtNumberPressed;
+        internal TextBox txtInstances;
+        internal TextBox txtOutput1;
+        internal Button btnSubmit;
+        internal Button btnThree;
+        internal Button btnTwo;
+        internal Button btnOne;
+        internal PictureBox PictureBox1;
+    }
+}
---
+++ PhoneProjectC/frmOldPhone.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PhoneProjectC
+{
+    public partial class frmOldPhone : Form
+    {
+        String strCharacter;
+        int second = 0;
+
+        public frmOldPhone()
+        {
+            InitializeComponent();
+        }
+
+        public static string OldPhonePad(string strOutput)
+        {
+            return strOutput;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            //Execution of code is driven mostly by timer as this is dependent on
+            //how long you wait for User to decide on next character and which button he chooses
+            second += 1;
+            if (second >= 1)
+            { Timer1.Stop(); }
+            //Check which character User busy with at this point and to be used
+            GetChar(Int16.Parse(txtNumberPressed.Text), Int16.Parse(txtInstances.Text));
+            //Execute by setting variable (output1 textbox) at this stage 
+            txtOutput1.Text += OldPhonePad(strCharacter);
+            //Now start over by resetting defaults, and wait for next inputs from User
+            ResetDefaults();
+        }
+
+        private void frmOldPhone_Load(object sender, EventArgs e)
+        {
+            Timer1.Interval = 1000;//interval set at 1 second as per requirement for pause
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //This button assumes User is happy to Submit his message.  Once clicked, the message will be sent as final like a mobile phone operates currently
+            txtOutput1.Text += OldPhonePad(strCharacter);
+            ResetDefaults();//Reset which button was clicked, and restart the string buildup of characters
+            txtOutput2.Text += txtOutput1.Text;//Curretnly retaining existing Output Screen, but can merely reset if neccessary
+            txtOutput1.Text = "";//Start over to build new variable string after Submitting to screen
+        }
+
+       
+
+        private void btnBackspace_Click(object sender, EventArgs e)
+        {
+            //This controls backspace of variable string buildup, not Output Screen.  Once you submit on Output Screen you cant Edit, like a mobile would do
+            string strOutput1;
+            strOutput1 = txtOutput1.Text;
+        if(strOutput1.Length > 0)
+            { 
+                txtOutput1.Text = strOutput1.Substring(0, strOutput1.Length - 1);
+             }  
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            txtOutput1.Text += " ";
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("0");
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("1");
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("2");
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("3");
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("4");
+        }
+
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("5");
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("6");
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("7");
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();// 'Timer starts functioning
+            SetButtonClicked("8");
+        }
+
+        protected void GetChar(Int16 intButton, Int16 intChar)
+        {
+            string[,] values = new string[9, 4] {
+                           {"&", "'", "(", ""},
+                           { "a", "b", "c", ""},
+                           { "d", "e", "f", ""},
+                           { "g", "h", "i", ""},
+                           { "j", "k", "l", ""},
+                           { "m", "n", "o", ""},
+                           { "p", "q", "r", "s"},
+                           { "t", "u", "v", ""},
+                           { "w", "x", "y", "z"}
+            };
+
+            //Get bounds of the array.
+            int bound0 = values.GetUpperBound(0);
+            int bound1 = values.GetUpperBound(1);
+            bound0 += 1;
+            bound1 += 1;
+            //Loop over all elements.
+            for (int i = 0; i < bound0; i++)
+            {
+                for (int j = 0; j < bound1; j++)
+                {
+                    //Get element for button character selection.
+                    if (i == intButton && j == intChar)
+                    {
+                        //string strChar = values[i, j];
+                        strCharacter = values[i, j];
+                    }
+                }
+            }
+        }
+
+        protected void ResetDefaults()
+        {
+            txtInstances.Text = "";
+            txtNumberPressed.Text = "0";
+            strCharacter = "";
+        }
+
+        protected void SetButtonClicked(string strButtonPressed)
+        {
+            int num;
+            //Record which button was pressed, and control instances 
+            txtNumberPressed.Text = strButtonPressed;// 'button pressed. Should be one less than name, for index
+
+            if (txtInstances.Text.Length == 0)
+            { txtInstances.Text = "0"; }
+            else
+            {
+                if (Int16.Parse(txtInstances.Text) < 3)
+                {
+                    num = Int16.Parse(txtInstances.Text);
+                    num += 1;
+                    txtInstances.Text = num.ToString();
+                }
+            }
+        }
+    }
+}
